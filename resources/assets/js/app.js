a7fe0faa68039adcdeeaf2f1a07add9fda272b8b
Дмитрий Абраменко < >
2018-02-05 00:01:52 +0600

    bla bla commit

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,22 +1,21 @@
+import './bootstrap';
 
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import '../css/app.css'
+import configureStore from './store/configureStore'
 
-require('./bootstrap');
+import App from './containers/App'
 
-window.Vue = require('vue');
+const store = configureStore();
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
-
-const app = new Vue({
-    el: '#app'
-});
+ReactDOM.render(
+    <Provider store={store}>
+        <div className="app">
+            <App />
+        </div>
+    </Provider>,
+    document.getElementById('root')
+);
